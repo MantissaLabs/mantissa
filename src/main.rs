c351545c55a2e61e7fb572025b44610ea4dc2b76
Alexandre Beslic <ab21c8b9f7@abronan.com>
2025-04-21 16:01:27 +0100

    Add aya dependency, add basic info command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate clap;
 extern crate log;
+extern crate sysinfo;
 
 mod cli;
 pub mod container;
@@ -18,6 +19,7 @@ use merkle_search_tree::builder::Builder;
 use merkle_search_tree::MerkleSearchTree;
 use redb::{Database, TableDefinition};
 use std::error::Error;
+use sysinfo::{Components, Disks, Networks, System};
 
 use crate::hash_mvreg::HashableMVReg;
 
@@ -92,6 +94,57 @@ async fn main() -> Result<(), Box<dyn Error>> {
             let server = server::Server::new();
 
             server.start().await;
+        }
+        Some(("info", _)) => {
+            // Please note that we use "new_all" to ensure that all lists of
+            // CPUs and processes are filled!
+            let mut sys = System::new_all();
+
+            // First we update all information of our `System` struct.
+            sys.refresh_all();
+
+            println!("=> system:");
+            // RAM and swap information:
+            println!("total memory: {} bytes", sys.total_memory());
+            println!("used memory : {} bytes", sys.used_memory());
+            println!("total swap  : {} bytes", sys.total_swap());
+            println!("used swap   : {} bytes", sys.used_swap());
+
+            // Display system information:
+            println!("System name:             {:?}", System::name());
+            println!("System kernel version:   {:?}", System::kernel_version());
+            println!("System OS version:       {:?}", System::os_version());
+            println!("System host name:        {:?}", System::host_name());
+
+            // Number of CPUs:
+            println!("NB CPUs: {}", sys.cpus().len());
+
+            // We display all disks' information:
+            println!("=> disks:");
+            let disks = Disks::new_with_refreshed_list();
+            for disk in &disks {
+                println!("{disk:?}");
+            }
+
+            // Network interfaces name, total data received and total data transmitted:
+            let networks = Networks::new_with_refreshed_list();
+            println!("=> networks:");
+            for (interface_name, data) in &networks {
+                println!(
+                    "{interface_name}: {} B (down) / {} B (up)",
+                    data.total_received(),
+                    data.total_transmitted(),
+                );
+                // If you want the amount of data received/transmitted since last call
+                // to `Networks::refresh`, use `received`/`transmitted`.
+            }
+
+            // Components temperature:
+            let components = Components::new_with_refreshed_list();
+            println!("=> components:");
+            for component in &components {
+                println!("{component:?}");
+            }
         }
         Some(("link", _)) => {
             // Creating an MVReg and store a value in there.
