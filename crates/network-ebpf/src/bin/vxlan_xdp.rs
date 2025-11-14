e69ef2dc3cc36ce8ae5de55b01b78596531a8eb9
Alexandre Beslic <ab21c8b9f7@abronan.com>
2025-11-14 13:35:25 +0000

    networking: fix broken connectivity because of wrong filtering

--- crates/network-ebpf/src/bin/vxlan_xdp.rs
+++ crates/network-ebpf/src/bin/vxlan_xdp.rs
@@ -10,13 +10,13 @@ use aya_ebpf::{
     programs::XdpContext,
 };
 use network_ebpf::{
-    net::{self, EthernetHeader, Ipv4Header, UdpHeader, ETH_HDR_LEN},
+    net::{self, EthernetHeader},
     stats::{self, PacketStats},
 };
 
-const VXLAN_PORT: u16 = 4789;
 const ETH_P_IPV4: u16 = 0x0800;
-const IPPROTO_UDP: u8 = 17;
+const ETH_P_IPV6: u16 = 0x86dd;
+const ETH_P_ARP: u16 = 0x0806;
 
 #[map(name = "VXLAN_STATS")]
 static mut VXLAN_STATS: PerCpuArray<PacketStats> = PerCpuArray::with_max_entries(1, 0);
@@ -41,25 +41,11 @@ fn validate_vxlan(ctx: &XdpContext) -> Result<(), ()> {
     let data = ctx.data();
     let data_end = ctx.data_end();
     let eth: EthernetHeader = unsafe { net::read_at(data, data_end, 0)? };
-    if eth.protocol() != ETH_P_IPV4 {
+    let proto = eth.protocol();
+    if proto != ETH_P_IPV4 && proto != ETH_P_IPV6 && proto != ETH_P_ARP {
         return Err(());
     }
-
-    let ipv4: Ipv4Header = unsafe { net::read_at(data, data_end, ETH_HDR_LEN)? };
-    if ipv4.version() != 4 || ipv4.protocol != IPPROTO_UDP {
-        return Err(());
-    }
-    if ipv4.is_fragmented() {
-        return Err(());
-    }
-    let ip_header_len = ipv4.header_len();
-    if ip_header_len < 20 {
-        return Err(());
-    }
-
-    let udp_offset = ETH_HDR_LEN + ip_header_len;
-    let udp: UdpHeader = unsafe { net::read_at(data, data_end, udp_offset)? };
-    if udp.dest_port() != VXLAN_PORT {
+    if !net::is_unicast(&eth.source()) {
         return Err(());
     }
 
